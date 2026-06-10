c8b8a039a38fcc8047660d950b6d13f491f3ad97
fperillo <fperillo@gmail.com>
2026-06-10 16:06:43 +0200

    environment settings

---
+++ set_hb_5_10_1.bat
@@ -0,0 +1,7 @@
+set MINGW_COMPILER=C:\Qt\Qt5.10.1\Tools\mingw530_32\bin
+set QT_HOME_DIR=C:\Qt\Qt5.10.1\5.10.1\mingw53_32
+set HARBOUR_HOME_DIR=c:\cvs\harbour-5_10_1\bin\win\mingw
+
+set PATH=%MINGW_COMPILER%;%QT_HOME_DIR%\bin;%HARBOUR_HOME_DIR%;%PATH%
+set HB_WITH_QT=%QT_HOME_DIR%\include
+
